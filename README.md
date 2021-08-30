b3c3ffdede54cb0e0392971205010843f33f2634
Jörn Neumeyer <contact@joern-neumeyer.de>
2021-08-30 12:54:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# remote audio control
+A very simplistic, web-based remote audio control.
+Clone/download this repository and run `npm start` to start it.
+Next, visit the host machine via `<some-local-ip-address>:8081`.
+Now you should see the remote control.
+just use the slider and the volume will be adjusted on the host!
---
+++ index.ts
@@ -0,0 +1,36 @@
+import * as express from 'express';
+import * as path    from 'path';
+import * as loudness from 'loudness';
+import bodyParser from 'body-parser';
+
+const app = express.default();
+
+const endpoint = {
+  host: '0.0.0.0',
+  port: 8081,
+}
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+
+app.put('/volume', async (req  , res) => {
+  console.log('volume request body', req.body);
+  const volume = parseInt(req.body.volume);
+  if (isNaN(volume)) {
+    res.status(400).end();
+    return;
+  }
+  await loudness.setVolume(volume);
+  console.log('updated volume to level', volume);
+  res.status(200).end('ok');
+});
+
+app.get('/volume', async (_req, res) => {
+  const volume = await loudness.getVolume();
+  res.json(volume).end();
+});
+
+app.listen(endpoint.port, endpoint.host, () => {
+  console.log(`server is running on ${endpoint.host}:${endpoint.port}`);
+});
